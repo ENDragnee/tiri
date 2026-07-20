ac8201ae68081061f2bdbfce11f6a5529b0653ea
FIBA00 <fraolbulti0@gmail.com>
2026-07-20 10:23:46 +0300

    - feat: created new readme page - created initial sign up and login pages - created create invitation page - added shadcn components - added navbar  and editied the landing page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,13 @@
+
+import {useState} from "react"
+
+export default function Login() {
+    const [name, setName] = useState("")
+
+
+    return (
+        <div>
+            Login to use the app
+        </div>
+    );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <header>
+      <nav className="bg-gray-800">hi this is nav bar in tsx</nav>
+    </header>
+  );
+}
